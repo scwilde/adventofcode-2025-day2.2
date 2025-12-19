27be88d421c7ebd23c45d418c546bafacaee9036
scwilde <smuelwilde926@gmail.com>
2025-12-19 15:06:27 -0800

    Separate the puzzle setup from the solution benchmarks

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,18 @@ mod puzzle;
 mod utils;
 mod id;
 
-fn benchmark<S: AsRef<str>>(msg: S, solve_method: fn(u64) -> bool, cli: &cli::Cli) -> Result<(), Box<dyn Error>> {
+fn benchmark<S: AsRef<str>>(cli: &cli::Cli, msg: S, solve_method: fn(u64) -> bool, data: &puzzle::Data) -> Result<(), Box<dyn Error>> {
     println!("{}", msg.as_ref());
     
     let timer = Instant::now();
-    match (cli.expected_invalid_id_ct, cli.expected_invalid_id_sum) == puzzle::solve(cli, solve_method)? {
+    match (cli.expected_invalid_id_ct, cli.expected_invalid_id_sum) == puzzle::solve(cli, data, solve_method) {
         true => { println!("Puzzle solved correctly in {}s without black box", timer.elapsed().as_secs_f32()) }
         false => { println!("Puzzle solved incorrectly in {}s without black box", timer.elapsed().as_secs_f32()) }
     }
 
     let timer = Instant::now();
     let solution = std::hint::black_box((cli.expected_invalid_id_ct, cli.expected_invalid_id_sum));
-    let answer = std::hint::black_box(puzzle::solve(cli, solve_method)?);
+    let answer = std::hint::black_box(puzzle::solve(cli, data, solve_method));
     match solution == answer {
         true => { println!("Puzzle solved correctly in {}s with black box", timer.elapsed().as_secs_f32()) }
         false => { println!("Puzzle solved incorrectly in {}s with black box", timer.elapsed().as_secs_f32()) }
@@ -29,31 +29,37 @@ fn benchmark<S: AsRef<str>>(msg: S, solve_method: fn(u64) -> bool, cli: &cli::Cl
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = cli::Cli::parse();
+    let data = puzzle::setup(&cli)?;
 
     benchmark(
+        &cli,
         "Benchmarking my (bad) solution...",
         id::is_id_valid_my,
-        &cli
+        &data
     )?;
     benchmark(
+        &cli,
         "Benchmarking regular Vec rotation...",
         id::is_id_valid_vec,
-        &cli
+        &data
     )?;
     benchmark(
+        &cli,
         "Benchmarking VecDeque rotation...",
         id::is_id_valid_vecdeque,
-        &cli
+        &data
     )?;
     benchmark(
+        &cli,
         "Benchmarking kyuuhachi's string slice solution...",
         id::is_id_valid_kyuu,
-        &cli
+        &data
     )?;
     benchmark(
+        &cli,
         "Benchmarking orlp's numeric solution...",
         id::is_id_valid_orlp,
-        &cli
+        &data
     )?;
 
     Ok(())
--- src/puzzle.rs
+++ src/puzzle.rs
@@ -4,50 +4,57 @@ use std::fs;
 use crate::cli;
 use crate::utils;
 
-pub fn solve(cli: &cli::Cli, is_id_valid: fn(u64) -> bool) -> Result<(u64, u64), Box<dyn Error>> {
-    cli.log_at_level(1, "Reading puzzle input...");
-    let data = fs::read_to_string(&cli.input_path)?;
+pub struct Data {
+    parsed_range_count: i32,
+    parsed_id_count: i32,
+    ids: Vec<u64>
+}
 
-    cli.log_at_level(1, "Parsing ID Ranges...");
-    let mut parsed_range_count = 0;
-    let mut total_parsed_id_count = 0;
-    let mut total_invalid_id_count = 0;
-    let mut invalid_id_sum = 0;
-    for id_range in data.split(",") {
-        cli.log_at_level(2, format!("Parsing {}...", id_range));
+pub fn setup(cli: &cli::Cli) -> Result<Data, Box<dyn Error>> {
+    cli.log_at_level(2, "Reading puzzle input...");
+    let input = fs::read_to_string(cli.input_path.clone())?;
+
+    cli.log_at_level(2, "Parsing ID Ranges...");
+    let mut data = Data{
+        parsed_range_count: 0,
+        parsed_id_count: 0,
+        ids: Vec::new()
+    };
+    for id_range in input.split(",") {
+        cli.log_at_level(3, format!("Parsing {}...", id_range));
         let id_range = utils::Range::from_string(id_range)?;
-
-        cli.log_at_level(2, format!(
-            "Searching range {}..={} for invalid IDs...",
-            id_range.lower, id_range.upper
-        ));
-        let mut range_invalid_id_count = 0;
-        let mut range_parsed_id_count = 0;
+        let mut range_id_count = 0;
         for id in id_range.lower..=id_range.upper {
-            total_parsed_id_count += 1;
-            range_parsed_id_count += 1;
-            match is_id_valid(id) {
-                false => {
-                    cli.log_at_level(3, format!("{} is not a valid ID!", id));
-                    range_invalid_id_count += 1;
-                    total_invalid_id_count += 1;
-                    invalid_id_sum += id;
-                }
-                true => cli.log_at_level(3, format!("{} is a valid ID!", id)),
-            }
+            data.ids.push(id);
+            data.parsed_id_count += 1;
+            range_id_count += 1;
+            cli.log_at_level(4, format!("Queing {}...", id));
         }
-        cli.log_at_level(2, format!(
-            "Found {} invalid IDs out of {} total. New invalid ID sum {}",
-            range_invalid_id_count, range_parsed_id_count, invalid_id_sum
+        cli.log_at_level(3, format!(
+            "Queued {} IDs from range {}..={}",
+            range_id_count, id_range.lower, id_range.upper
         ));
-        parsed_range_count += 1;
+        data.parsed_range_count += 1;
     }
 
-    cli.log_at_level(1, format!(
-        "Parsed {} ID ranges and found {} invalid IDs out of {} total. Invalid ID sum:",
-        parsed_range_count, total_invalid_id_count, total_parsed_id_count
-    ));
+    Ok(data)
+}
 
-    Ok((total_invalid_id_count, invalid_id_sum))
+pub fn solve(cli: &cli::Cli, data: &Data, is_id_valid: fn(u64) -> bool) -> (u64, u64) {
+    let mut invalid_id_count = 0;
+    let mut invalid_id_sum = 0;
+    
+    for id in data.ids.iter() {
+        match is_id_valid(*id) {
+            false => {
+                cli.log_at_level(2, format!("{} is not a valid ID!", id));
+                invalid_id_count += 1;
+                invalid_id_sum += id;
+            }
+            true => cli.log_at_level(2, format!("{} is a valid ID!", id))
+        }
+    }
+
+    (invalid_id_count, invalid_id_sum)
 }
 
